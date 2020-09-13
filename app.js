aaf484939bce42063283c63d9cb569b5071b851f
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-09-13 12:28:47 +0700

    add ngrok, test webhook

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
-
+const port = 8080;
+const ngrok = require('ngrok');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,9 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(port, (err) => {
+    if (err) return console.log(`Something bad happened`);
 
-app.listen(3000, function() {
-    console.log("Server running at port " + 3000);
+    ngrok.connect(port, function(err, url) {
+        if (err) {
+            console.log(`Something bad happened at ${err}`);
+            return;
+        }
+        console.log(`Node.js local server is publicly-accessible at ${url}`);
+    });
+    console.log(`Node.js server listening on ${port}`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send("index");
+    console.log('A get request');
+    res.send("get index");
+});
+router.post('/', function(req, res, next) {
+    console.log('A post request');
+    res.send("post index");
 });
 
 module.exports = router;
